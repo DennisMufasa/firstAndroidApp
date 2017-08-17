7030d75ca2866567ae46ec7123e6baa27c2346f0
DennisMufasa <denny.muasa@gmail.com>
2017-08-17 13:23:08 +0300

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+FirstAppOnTabs
+``This is a simple android app that displays items on a listview.``
+``The listView is on a fragment. The mainActivity fragment.``
+``The items have an arrow icon and an OnOptionItemSelectedListener.``
---
+++ src/main/java/com/example/mufasa/lesson3/Contacts.java
@@ -0,0 +1,40 @@
+package com.example.mufasa.lesson3;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public  class Contacts extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public Contacts() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static MainActivity.PlaceholderFragment newInstance(int sectionNumber) {
+        MainActivity.PlaceholderFragment fragment = new MainActivity.PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        return rootView;
+    }
+}
